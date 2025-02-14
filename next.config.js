1867874939fb02068a7273641f23dab75adab354
fujitatsu <22fujii.tmr@gmail.com>
2025-02-15 02:17:19 +0900

    変更

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     output: 'standalone',
     env: {
         // Reference a variable that was defined in the .env file and make it available at Build Time
-        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+        // NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
       },
 }
 
